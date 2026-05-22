0d4302f7b22c11075b4de33b557c4f2ebe78c67b
Hchnr <hechenrui123@gmail.com>
2026-05-22 16:06:25 +0800

    format: with ruff format .

--- tests/integration/ops/conftest.py
+++ tests/integration/ops/conftest.py
@@ -1,5 +1,3 @@
-
-
 def pytest_configure(config):
     config.addinivalue_line("markers", "anyplatform: runs on any platform")
     config.addinivalue_line("markers", "cuda: requires CUDA platform")
